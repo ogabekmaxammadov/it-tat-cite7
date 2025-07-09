0b1aa64d6186569865bec475b0425808c5d4d6ea
Ogabek Maxammadov <163625412+ogabekmaxammadov@users.noreply.github.com>
2025-07-09 16:12:41 +0500

    yangilandi

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,6 @@
 // const OnlineCourses = lazy(() => import("./pages/onlineCourses/OnlineCourses"));
 // const NotFound = lazy(() => import("./pages/notFound/NotFound"));
 
-
 // const App = () => (
 //   <>
 //     <Header />
@@ -37,73 +36,77 @@
 //         <Route path="RoyxatdanOtish" element={<RoyxatdanOtish />} />
 //       </Routes>
 //     </Suspense>
-    
+
 //     <Footer />
 //   </>
 // );
 
 // export default App;
-import React, { Suspense, lazy } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Suspense, lazy } from 'react'
+import { Route, Routes } from 'react-router-dom'
 
-import CoursesAndPrice from "./pages/coursesAndPrice/CursesAndPrice";
-import Registration from "./pages/boshSahifa/registration/Registration";
-import RoyxatdanOtish from "./pages/clickmodal/RoyxatdanOtish";
-import Loading from "./Loading";
-import Header from "./pages/boshSahifa/header/Header";
-import Footer from "./pages/boshSahifa/footer/Footer";
+import Loading from './Loading'
+import Footer from './pages/boshSahifa/footer/Footer'
+import Header from './pages/boshSahifa/header/Header'
+import Registration from './pages/boshSahifa/registration/Registration'
+import RoyxatdanOtish from './pages/clickmodal/RoyxatdanOtish'
+import CoursesAndPrice from './pages/coursesAndPrice/CursesAndPrice'
 
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
-import "./App.css";
+import './App.css'
+
+import 'antd/dist/reset.css'
 
 // Lazy load sahifalar
-const Kurslar = lazy(() => import("./pages/coursesAndPrice/Kurslar"));
-const ProgrammingCourse = lazy(() => import("./pages/programmingCourses/ProgrammingCourse"));
-const LichTeacher = lazy(() => import("./pages/lichTeachers/LichTeacher"));
-const Teachers = lazy(() => import("./pages/Teachers/Teachers"));
-const BoshSahifa = lazy(() => import("./pages/boshSahifa/BoshSahifa"));
-const OnlineCourses = lazy(() => import("./pages/onlineCourses/OnlineCourses"));
-const NotFound = lazy(() => import("./pages/notFound/NotFound"));
+const Kurslar = lazy(() => import('./pages/coursesAndPrice/Kurslar'))
+const ProgrammingCourse = lazy(() =>
+	import('./pages/programmingCourses/ProgrammingCourse')
+)
+const LichTeacher = lazy(() => import('./pages/lichTeachers/LichTeacher'))
+const Teachers = lazy(() => import('./pages/Teachers/Teachers'))
+const BoshSahifa = lazy(() => import('./pages/boshSahifa/BoshSahifa'))
+const OnlineCourses = lazy(() => import('./pages/onlineCourses/OnlineCourses'))
+const NotFound = lazy(() => import('./pages/notFound/NotFound'))
 
 const App = () => {
-  return (
-    <>
-      <Header />
+	return (
+		<>
+			<Header />
 
-      <Suspense fallback={<Loading />}>
-        <Routes>
-          <Route path="/" element={<BoshSahifa />} />
-          <Route path="/ustozlar" element={<Teachers />} />
-          <Route path="/kurslar" element={<Kurslar />} />
-          <Route path="/online-kurslar" element={<OnlineCourses />} />
-          <Route path="/courseAndPrice" element={<CoursesAndPrice />} />
-          <Route path="/ustozlar/ustoz/:id" element={<LichTeacher />} />
-          <Route path="/kurslar/kurs/:id" element={<ProgrammingCourse />} />
-          <Route path="/registration" element={<Registration />} />
-          <Route path="/RoyxatdanOtish" element={<RoyxatdanOtish />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Suspense>
+			<Suspense fallback={<Loading />}>
+				<Routes>
+					<Route path='/' element={<BoshSahifa />} />
+					<Route path='/ustozlar' element={<Teachers />} />
+					<Route path='/kurslar' element={<Kurslar />} />
+					<Route path='/online-kurslar' element={<OnlineCourses />} />
+					<Route path='/courseAndPrice' element={<CoursesAndPrice />} />
+					<Route path='/ustozlar/ustoz/:id' element={<LichTeacher />} />
+					<Route path='/kurslar/kurs/:id' element={<ProgrammingCourse />} />
+					<Route path='/registration' element={<Registration />} />
+					<Route path='/RoyxatdanOtish' element={<RoyxatdanOtish />} />
+					<Route path='*' element={<NotFound />} />
+				</Routes>
+			</Suspense>
 
-      <Footer />
+			<Footer />
 
-      {/* Global ToastContainer */}
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={true}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss={false}
-        draggable
-        pauseOnHover
-        theme="light"
-      />
-    </>
-  );
-};
+			{/* Global ToastContainer */}
+			<ToastContainer
+				position='top-right'
+				autoClose={3000}
+				hideProgressBar={false}
+				newestOnTop={true}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss={false}
+				draggable
+				pauseOnHover
+				theme='light'
+			/>
+		</>
+	)
+}
 
-export default App;
+export default App
